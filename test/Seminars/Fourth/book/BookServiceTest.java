e6042765116456b06d5892ef5fd69f629cb6e093
Lidiya <lesnikoval@yandex.ru>
2023-10-04 12:04:18 +0300

    dz1

---
+++ test/Seminars/Fourth/book/BookServiceTest.java
@@ -0,0 +1,33 @@
+package Seminars.Fourth.book;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class BookServiceTest {
+    /**
+     * У вас есть класс BookService, который использует интерфейс BookRepository для получения
+     * информации о книгах из базы данных. Ваша задача написать unit-тесты для BookService,
+     * используя Mockito для создания мок-объекта BookRepository.
+     */
+    @Test
+    void testBookService(){
+        BookRepository bookRepositoryMock = mock(InMemoryBookRepository.class);
+        List<Book> books = new ArrayList<Book>();
+        books.add(new Book("1", "Name1", "Autor1"));
+        books.add(new Book("2", "Name2", "Autor2"));
+
+        when(bookRepositoryMock.findAll()).thenReturn(books);
+        when(bookRepositoryMock.findById("1")).thenReturn(books.get(0));
+
+        BookService bookService = new BookService(bookRepositoryMock);
+
+        assertThat(bookService.findAllBooks()).isEqualTo(books);
+        assertThat(bookService.findBookById("1")).isEqualTo(books.get(0));
+    }
+}
